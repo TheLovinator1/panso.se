a92c8f48032d2bfab01be1231f57ad3ff50a9302
Joakim Hellsén <tlovinator@gmail.com>
2024-03-13 21:22:08 +0100

    Use Typer in manage.py

--- manage.py
+++ manage.py
@@ -1,22 +1,83 @@
-#!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
+from __future__ import annotations
 
 import os
-import sys
+from datetime import datetime
+
+import django
+import typer
+from django.core.management import call_command
+from rich import print
+
+# Set the Django settings module
+os.environ.setdefault(key="DJANGO_SETTINGS_MODULE", value="panso.settings")
+django.setup()
+
+app = typer.Typer(
+    name="panso",
+    help="The panso.se management utility.",
+    add_completion=False,
+    no_args_is_help=True,
+    pretty_exceptions_enable=True,
+)
 
 
-def main() -> None:
-    """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "panso.settings")
-    try:
-        from django.core.management import execute_from_command_line  # noqa: PLC0415
-    except ImportError as exc:
-        msg = "Couldn't import Django. Did you forget to run 'poetry install' or 'poetry shell'?"
-        raise ImportError(
-            msg,
-        ) from exc
-    execute_from_command_line(sys.argv)
+@app.command()
+def migrate() -> None:
+    """Apply database migrations."""
+    # Take a backup of the database before migrating
+    file_safe_datetime = datetime.now(tz=datetime.now().astimezone().tzinfo).strftime(
+        "%Y-%m-%d_%H-%M-%S",
+    )
+    call_command(
+        command_name="dumpdata",
+        output=f"data/backup/{file_safe_datetime}.json.gz",
+        format="json",
+    )
+    call_command(command_name="migrate")
+    print("[green]Migrations applied.[/green]")
+
+
+@app.command()
+def runserver() -> None:
+    """Run the development server."""
+    migrate()
+
+    call_command(command_name="runserver")
+
+
+@app.command()
+def serve() -> None:
+    """Alias for runserver."""
+    runserver()
+
+
+@app.command()
+def createsuperuser() -> None:
+    """Create a superuser."""
+    call_command(command_name="createsuperuser")
+    print("[green]Superuser created.[/green]")
+
+
+@app.command()
+def collectstatic() -> None:
+    """Collect static files."""
+    call_command(command_name="collectstatic", interactive=False)
+
+
+@app.command()
+def test() -> None:
+    """Run tests."""
+    call_command(command_name="test")
+    print("[green]Tests passed.[/green]")
+
+
+@app.command()
+def makemigrations() -> None:
+    """Make migrations."""
+    call_command(command_name="makemigrations")
+    print("[green]Migrations created.[/green]")
+    print("[yellow]Please run `migrate` to apply the migrations.[/yellow]")
 
 
 if __name__ == "__main__":
-    main()
+    app()
