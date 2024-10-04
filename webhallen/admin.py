adab5ec2276e03e4330971d7003166543b7c402a
Joakim Hellsén <tlovinator@gmail.com>
2024-10-05 00:31:39 +0200

    Update admin.py to register all the Webhallen models

--- webhallen/admin.py
+++ webhallen/admin.py
@@ -1,1 +1,25 @@
-# Register your models here.
+from __future__ import annotations
+
+from django.contrib import admin
+
+from .models import (
+    SitemapArticle,
+    SitemapCampaign,
+    SitemapCategory,
+    SitemapHome,
+    SitemapInfoPages,
+    SitemapManufacturer,
+    SitemapProduct,
+    SitemapSection,
+    WebhallenProductJSON,
+)
+
+admin.site.register(SitemapArticle)
+admin.site.register(SitemapCampaign)
+admin.site.register(SitemapCategory)
+admin.site.register(SitemapHome)
+admin.site.register(SitemapInfoPages)
+admin.site.register(SitemapManufacturer)
+admin.site.register(SitemapProduct)
+admin.site.register(SitemapSection)
+admin.site.register(WebhallenProductJSON)
